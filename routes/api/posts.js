9c245d252ffb5be33efbbdf7ef0551a4d1e27268
Unknown <deckersjoey@gmail.com>
2019-01-22 21:51:38 +0100

    Can create posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Posts');
+
+const validatePostInput = require('../validation/post');
+
 
 // @route   GET api/posts/tests
 // @desc    tests post route
 // @access  public 
 router.get('/test', (req, res) => res.json({msg: "Posts Works"}));
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  privte 
+router.post('/', passport.authenticate('jwt', {session:false}), (req, res) => {
+    const { errors, isValid} = validatePostInput(req.body);
+
+    if(!isValid){
+        return res.status(400).json(errors);
+    }
+    
+    const newPost = new Post({ 
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.name,
+        user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+});
+
 
 module.exports = router;
